d6190f3d119fd3a1db5a01a66ec6b3717c6ea142
TatsukiIshijima <tatsuki445@gmail.com>
2025-04-06 21:51:55 +0900

    setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +14,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // eslint-plugin-prettierを明示的に追加
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+
+  // Prettierと競合しやすいESlitのルールを無効化
+  {
+    rules: prettierConfig.rules,
+  },
 ];
 
 export default eslintConfig;
