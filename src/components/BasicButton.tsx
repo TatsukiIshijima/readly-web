00b0eca0e0e430452d1cbda849ad869f31f9207f
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-08 14:55:54 +0900

    create basic button

---
+++ src/components/BasicButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@mui/material';
+
+interface BasicButtonProps {
+  onClick: () => void;
+  id: string;
+  label: string;
+  disabled: boolean;
+  fullWidth: boolean;
+}
+
+export default function BasicButton({
+  onClick,
+  id = 'basic-button',
+  label = 'Button',
+  disabled = false,
+  fullWidth = true,
+}: BasicButtonProps) {
+  return (
+    <Button
+      onClick={() => {
+        onClick();
+      }}
+      id={id}
+      variant={'contained'}
+      disabled={disabled}
+      fullWidth={fullWidth}
+    >
+      {label}
+    </Button>
+  );
+}
