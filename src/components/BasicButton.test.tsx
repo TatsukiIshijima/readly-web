4f9ad5c5ca901f8328bf1022b065722b22c1a92a
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-08 15:07:44 +0900

    add test

---
+++ src/components/BasicButton.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import BasicButton from '@/components/BasicButton';
+
+describe('BasicButton', () => {
+  const setup = (propsOverrides = {}) => {
+    const defaultProps = {
+      onClick: jest.fn(),
+      id: 'basic-button',
+      label: 'Button',
+      disabled: false,
+      fullWidth: true,
+    };
+    const props = { ...defaultProps, ...propsOverrides };
+    render(<BasicButton {...props} />);
+  };
+  test('renders correctly with default props', () => {
+    setup();
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('Button');
+    expect(button).not.toBeDisabled();
+  });
+
+  test('call onClick when button is clicked', () => {
+    const onClick = jest.fn();
+    setup({ onClick: onClick });
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('does not call onClick when button is disabled', () => {
+    const onClick = jest.fn();
+    setup({ onClick: onClick, disabled: true });
+    const button = screen.getByRole('button');
+    expect(button).toBeDisabled();
+    fireEvent.click(button);
+    expect(onClick).not.toHaveBeenCalled();
+  });
+});
