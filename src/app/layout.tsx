d2124439bb51ac4927f6978994ff2c46278de3b5
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-09 12:08:36 +0900

    remove InitColorSchemeScript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/theme';
-import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-robot',
+});
 
 export default function RootLayout({
   children,
@@ -10,9 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={roboto.variable}>
       <body>
-        <InitColorSchemeScript attribute="class" />
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </AppRouterCacheProvider>
