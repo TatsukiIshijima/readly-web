0b019affe46cc31746c1f31188d7cbb99a67f262
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-09 18:25:21 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,6 @@ import React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/theme';
-import { Roboto } from 'next/font/google';
-
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-robot',
-});
 
 export default function RootLayout({
   children,
@@ -17,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" className={roboto.variable}>
+    <html lang="ja">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
