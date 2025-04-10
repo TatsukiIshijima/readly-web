ba5f432e1a02a73b04ab2537daea6ff65d4c72c9
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-10 16:32:45 +0900

    create book item test

---
+++ src/components/BookItem.test.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import BookItem from '@/components/BookItem';
+
+describe('BookItem', () => {
+  const setup = (propsOverrides = {}) => {
+    const defaultProps = {
+      id: 1,
+      title: 'Title',
+      onClick: jest.fn(),
+      imgUrl: 'https://sample/sample.jpg',
+      author: 'Author',
+    };
+    const props = { ...defaultProps, ...propsOverrides };
+    render(<BookItem {...props} />);
+  };
+
+  test('renders correctly with default props', () => {
+    setup();
+    expect(screen.getByText('Title')).toBeInTheDocument();
+    expect(screen.getByText('Author')).toBeInTheDocument();
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://sample/sample.jpg');
+    expect(img).toHaveAttribute('alt', 'Book Cover');
+  });
+
+  test('calls onClick when clicked', () => {
+    const onClick = jest.fn();
+    setup({ onClick: onClick });
+    const card = screen.getByRole('button');
+    fireEvent.click(card);
+    expect(onClick).toHaveBeenCalledWith(1);
+  });
+});
