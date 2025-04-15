36c4a0dd1683b40c8e391bea60fc06926b6c5870
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-15 13:51:25 +0900

    [wip] create register page

--- src/app/book/register/page.tsx
+++ src/app/book/register/page.tsx
@@ -1,11 +1,44 @@
-import { Box, Chip, Stack, Select, MenuItem } from '@mui/material';
+import {
+  Box,
+  Chip,
+  Stack,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+  OutlinedInput,
+} from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import BasicTextField from '@/components/BasicTextField';
 import BasicButton from '@/components/BasicButton';
+import React from 'react';
+import { SelectChangeEvent } from 'node_modules/@mui/material/Select';
+
+const dummyGenres = [
+  'genre1',
+  'genre2',
+  'genre3',
+  'genre4',
+  'genre5',
+  'genre6',
+  'genre7',
+  'genre8',
+  'genre9',
+  'genre10',
+];
 
 export default function BookRegister() {
+  const [genres, setGenres] = React.useState<string[]>([]);
+
+  const handleSelectChange = (event: SelectChangeEvent<typeof genres>) => {
+    const {
+      target: { value },
+    } = event;
+    setGenres(typeof value === 'string' ? value.split(',') : value);
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Stack component={'form'} method={'POST'} spacing={4}>
@@ -55,30 +88,45 @@ export default function BookRegister() {
           error={false}
           errorMessage={''}
         ></BasicTextField>
-        <Select
-          multiple
-          value={['dog']}
-          renderValue={(selected: string[]) => (
-            <Box sx={{ display: 'flex', gap: '0.25rem' }}>
-              {selected.map((selectedOption) => {
-                const option = selected.find((opt) => opt === selectedOption);
-                return (
-                  <Chip key={selectedOption} label={option || selectedOption} />
-                );
-              })}
-            </Box>
-          )}
-        >
-          <MenuItem value="dog">Dog</MenuItem>
-          <MenuItem value="cat">Cat</MenuItem>
-          <MenuItem value="fish">Fish</MenuItem>
-          <MenuItem value="bird">Bird</MenuItem>
-        </Select>
-        <Select defaultValue={'unread'}>
-          <MenuItem value={'unread'}>未読</MenuItem>
-          <MenuItem value={'reading'}>読書中</MenuItem>
-          <MenuItem value={'done'}>読了</MenuItem>
-        </Select>
+        <FormControl fullWidth={true}>
+          <InputLabel id={'genre-label'}>Genres</InputLabel>
+          <Select
+            labelId={'genre-label'}
+            id={'genre-select'}
+            multiple
+            value={genres}
+            onChange={handleSelectChange}
+            input={
+              <OutlinedInput id={'selected-multiple-chip'} label={'Genres'} />
+            }
+            renderValue={(selected: string[]) => (
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '0.5' }}>
+                {selected.map((value) => {
+                  return <Chip key={value} label={value} />;
+                })}
+              </Box>
+            )}
+          >
+            {dummyGenres.map((value) => (
+              <MenuItem key={value} value={value}>
+                {value}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth={true}>
+          <InputLabel id={'reading-status-label'}>Reading Status</InputLabel>
+          <Select
+            labelId={'reading-status'}
+            id={'reading-status-select'}
+            label={'Reading Status'}
+            defaultValue={'unread'}
+          >
+            <MenuItem value={'unread'}>Unread</MenuItem>
+            <MenuItem value={'reading'}>Reading</MenuItem>
+            <MenuItem value={'done'}>Done</MenuItem>
+          </Select>
+        </FormControl>
         <DatePicker label="開始日"></DatePicker>
         <DatePicker label="終了日"></DatePicker>
         <BasicButton
