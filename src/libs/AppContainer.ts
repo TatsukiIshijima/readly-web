0d150538226a4365c5d09b57a0e5414eb17a6786
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-21 13:36:08 +0900

    remove app container

--- src/libs/AppContainer.ts
+++
@@ -1,9 +0,0 @@
-import { ApiClient } from '@/libs/api/ApiClient';
-
-export class AppContainer {
-  apiClient: ApiClient;
-
-  constructor(apiClient: ApiClient) {
-    this.apiClient = apiClient;
-  }
-}
