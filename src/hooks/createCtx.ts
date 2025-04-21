0d5e36e75cacf4a548ee2534a96ad4ef84121d91
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-21 13:40:32 +0900

    add comment

--- src/hooks/createCtx.ts
+++ src/hooks/createCtx.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// 参考：https://qiita.com/johnmackay150/items/88654e5064290c24a32a
 export function createCtx<T>() {
   const ctx = React.createContext<T | undefined>(undefined);
   const useCtx = () => {
