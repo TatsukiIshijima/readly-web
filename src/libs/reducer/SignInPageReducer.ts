4ff2048a03fc0d1f1fe991938cd3d2cf2cfd58f0
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-21 17:15:29 +0900

    create sign in reducer

---
+++ src/libs/reducer/SignInPageReducer.ts
@@ -0,0 +1,54 @@
+export type SignInPageAction =
+  | { type: 'INPUT_EMAIL'; value: string }
+  | { type: 'INPUT_PASSWORD'; value: string }
+  | { type: 'VALIDATE_EMAIL'; error: string }
+  | { type: 'VALIDATE_PASSWORD'; error: string }
+  | { type: 'REQUEST_SIGN_IN' }
+  | { type: 'SUCCESS_SIGN_IN' }
+  | { type: 'FAILURE_SIGN_IN'; error: string };
+
+export type SignInPageState = {
+  email: string;
+  password: string;
+  emailValidateErrorMessage: string;
+  passwordValidateErrorMessage: string;
+  isRequesting: boolean;
+  signInErrorMessage: string;
+};
+
+export const initialSignInPageState: SignInPageState = {
+  email: '',
+  password: '',
+  emailValidateErrorMessage: '',
+  passwordValidateErrorMessage: '',
+  isRequesting: false,
+  signInErrorMessage: '',
+};
+
+export function signInPageReducer(
+  state: SignInPageState,
+  action: SignInPageAction
+): SignInPageState {
+  switch (action.type) {
+    case 'INPUT_EMAIL':
+      return { ...state, email: action.value };
+    case 'INPUT_PASSWORD':
+      return { ...state, password: action.value };
+    case 'VALIDATE_EMAIL':
+      return { ...state, emailValidateErrorMessage: action.error };
+    case 'VALIDATE_PASSWORD':
+      return { ...state, passwordValidateErrorMessage: action.error };
+    case 'REQUEST_SIGN_IN':
+      return { ...state, isRequesting: true };
+    case 'SUCCESS_SIGN_IN':
+      return { ...state, isRequesting: false };
+    case 'FAILURE_SIGN_IN':
+      return {
+        ...state,
+        isRequesting: false,
+        signInErrorMessage: action.error,
+      };
+    default:
+      throw Error('Unknown action: ' + action);
+  }
+}
