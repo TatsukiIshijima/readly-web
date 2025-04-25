1d152f6280d5f84fe3acd0cf78a661084f54df82
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-25 12:10:09 +0900

    setup storycap option

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,16 @@
 import type { Preview } from '@storybook/react';
+import { ScreenshotOptions, withScreenshot } from 'storycap';
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const options: ScreenshotOptions = {
+  delay: 100,
+  fullPage: false,
+  viewports: {
+    desktop: { width: 1920, height: 1080 },
+  },
+};
+
+export const decorators = [withScreenshot(options)];
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +21,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: MINIMAL_VIEWPORTS,
     },
   },
 };
