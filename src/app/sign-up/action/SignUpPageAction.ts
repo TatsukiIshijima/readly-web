2664cb355e175bad6834d3be331acaa374b7eed8
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-04-28 22:46:14 +0900

    apply validation

--- src/app/sign-up/action/SignUpPageAction.ts
+++ src/app/sign-up/action/SignUpPageAction.ts
@@ -1,5 +1,6 @@
 import { UserRepository } from '@/libs/repository/UserRepository';
 import React from 'react';
+import { validatePassword, validateUserName } from '@/libs/util/Validate';
 
 export type SignUpPageActionType =
   | { key: 'INPUT_USER_NAME'; value: string }
@@ -34,5 +35,50 @@ export class SignUpPageAction {
 
   inputPassword(value: string) {
     this.dispatch({ key: 'INPUT_PASSWORD', value });
+  }
+
+  private validateInputs(userName: string, email: string, password: string) {
+    let isValid = true;
+
+    if (validateUserName(userName)) {
+      this.dispatch({
+        key: 'VALIDATE_USER_NAME',
+        error: '',
+      });
+    } else {
+      this.dispatch({
+        key: 'VALIDATE_USER_NAME',
+        error: '5~30文字 記号は不可',
+      });
+      isValid = false;
+    }
+
+    if (validateUserName(email)) {
+      this.dispatch({
+        key: 'VALIDATE_EMAIL',
+        error: '',
+      });
+    } else {
+      this.dispatch({
+        key: 'VALIDATE_EMAIL',
+        error: '無効なメールアドレスの形式',
+      });
+      isValid = false;
+    }
+
+    if (validatePassword(password)) {
+      this.dispatch({
+        key: 'VALIDATE_PASSWORD',
+        error: '',
+      });
+    } else {
+      this.dispatch({
+        key: 'VALIDATE_PASSWORD',
+        error: '大小英数記号をそれぞれ1文字以上含む8~48文字',
+      });
+      isValid = false;
+    }
+
+    return isValid;
   }
 }
