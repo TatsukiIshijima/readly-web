dd962554daa74e4a1931cf8a12d018369fece653
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-05-02 11:54:33 +0900

    rename

--- src/libs/api/request/RegisterBookRequest.ts
+++ src/libs/api/request/RegisterBookRequest.ts
@@ -4,7 +4,7 @@ import {
 } from '@/types/ReadingStatus';
 import { Dayjs } from 'dayjs';
 import { RegisterBookRequest as protoRegisterBookRequest } from '@/libs/pb/rpc_register_book_pb';
-import { dayjsToTimestamp } from '@/libs/util/DayjsToTimestamp';
+import { dayjsToProtoDate } from '@/libs/util/DayjsMapper';
 
 export class RegisterBookRequest {
   title: string;
@@ -48,12 +48,12 @@ export class RegisterBookRequest {
   toProto(): protoRegisterBookRequest {
     const publishDate =
       this.publishDate !== null
-        ? dayjsToTimestamp(this.publishDate)
+        ? dayjsToProtoDate(this.publishDate)
         : undefined;
     const startDate =
-      this.startDate !== null ? dayjsToTimestamp(this.startDate) : undefined;
+      this.startDate !== null ? dayjsToProtoDate(this.startDate) : undefined;
     const endDate =
-      this.endDate !== null ? dayjsToTimestamp(this.endDate) : undefined;
+      this.endDate !== null ? dayjsToProtoDate(this.endDate) : undefined;
     return {
       $typeName: 'pb.RegisterBookRequest',
       title: this.title,
