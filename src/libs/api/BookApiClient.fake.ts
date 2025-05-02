ff1d5313ea1543b1f8a868d843361309d8930969
TatsukiIshijima <ishijima_tatsuki@abema.tv>
2025-05-02 11:54:47 +0900

    fix

--- src/libs/api/BookApiClient.fake.ts
+++ src/libs/api/BookApiClient.fake.ts
@@ -1,16 +1,16 @@
 import { BookApiClient } from '@/libs/api/BookApiClient';
 import { Book } from '@/libs/pb/book_pb';
-import { RegisterBookRequest } from '@/libs/pb/rpc_register_book_pb';
+import { RegisterBookRequest } from '@/libs/api/request/RegisterBookRequest';
+import { readingStatusDomainToProto } from '@/types/ReadingStatus';
 
 export class FakeBookApiClientImpl implements BookApiClient {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async register(request: RegisterBookRequest): Promise<Book> {
     const book: Book = {
       $typeName: 'pb.Book',
       id: BigInt(1),
       title: request.title,
       genres: request.genres,
-      readingStatus: request.readingStatus,
+      readingStatus: readingStatusDomainToProto(request.readingStatus),
     };
     return Promise.resolve(book);
   }
